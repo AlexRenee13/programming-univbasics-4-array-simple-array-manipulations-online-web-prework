4f9b2c300c5feeb4469dd11f188a94788c4f0fef
Alexis Palacios <alexis1994662@yahoo.com>
2019-10-27 23:17:20 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -5,9 +5,14 @@ def using_unshift(array, string)
   array.unshift("Staten Island")
 end
 def using_pop(array) 
-  small_dogs = array.pop
-  dog_breeds= array.pop
+  dog_breed = array.pop
 end
+
+def pop_with_args
+  small_breed = array.pop
+  dog_breed= array.pop
+end  
+
 def using_shift 
   im_so_over_this_city = array.shift
 end

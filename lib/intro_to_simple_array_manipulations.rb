b87c3f93209e2c1a1b66c33740f6cf2abc2b49bc
Alexis Palacios <alexis1994662@yahoo.com>
2019-10-27 23:58:57 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -22,17 +22,17 @@ def using_concat(array1, array2)
   array.concat= array1+array2
 end
 def using_insert(array)
-  array.insert[4]
+  array.insert
 end
 def using_uniq(array)
   array.uniq
 end
-def using_flatten(array)
+def using_flatten(array) 
   array.flatten 
 end
 def using_delete(array, string)
- array.delete= ["Steven"]
+ array.delete= []
 end
 def using_delete_at(array, integer)
-  array.delete= [2]
+  array.delete= []
 end

7b1375621a5a899bc5c580ef1584bff6dce19ed5
Alexis Palacios <alexis1994662@yahoo.com>
2019-10-28 00:07:21 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -31,8 +31,8 @@ def using_flatten(array)
   array.flatten 
 end
 def using_delete(array, string)
- array.delete ["Steven"]
+ instructors.delete ["Steven"]
 end
 def using_delete_at(array, integer)
-  array.delete [-1]
+  array.delete ["Robocop"]
 end

03273237319b4c141db297433c14b832ff4c3248
Alexis Palacios <alexis1994662@yahoo.com>
2019-10-28 01:20:48 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -21,7 +21,7 @@ end
 def using_concat(array)
    my_favorite_thing.concat 
 end
-def using_insert(array,element)
+def using_insert(array , element)
   array.insert(4 , element)
 end
 def using_uniq(array)
